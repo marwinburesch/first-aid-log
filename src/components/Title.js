96a2fdf7ff3ea597f0513ab61ffd8eb29113f78a
marwinburesch <marwin@posteo.de>
2019-07-25 17:07:18 +0200

    add Title component add docz for Title component

---
+++ src/components/Title.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const StyledTitle = styled.h1`
+  margin: 0px;
+  display: block;
+  font-size: 2em;
+  /* margin-top: 0.67em;
+     margin-bottom: 0.67em;
+     margin-left: 0;
+     margin-right: 0; */
+  font-weight: bold;
+`;
+
+export function Title({ text }) {
+  return <StyledTitle>{text}</StyledTitle>;
+}
+
+Title.propTypes = {
+  text: PropTypes.string.isRequired
+};
+
+Title.defaultProps = {
+  text: 'Enter Title Here'
+};
