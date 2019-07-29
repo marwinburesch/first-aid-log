6528990d55903f9dce51efda98e0f13030ca5524
marwinburesch <marwin@posteo.de>
2019-07-29 16:17:17 +0200

    delete docz

---
+++ src/components/List.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+export function List() {
+  return (
+    <>
+      <div>List</div>
+    </>
+  );
+}
